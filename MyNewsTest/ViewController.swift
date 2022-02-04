f052226f3e387f5909ecd901b98036341f9723f1
Victor <mr>
2022-02-04 19:02:31 +0300

    Initial Commit

---
+++ MyNewsTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyNewsTest
+//
+//  Created by Victor Mashukevich on 4.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
